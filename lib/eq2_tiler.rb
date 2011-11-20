b81b3a70e74e737f800b3c6c65a4e25ef15f408e
Peter Menke <pmenke@googlemail.com>
2011-11-20 17:34:51 +0100

    added todo comments to main file
    Signed-off-by: Peter Menke <pmenke@googlemail.com>

--- lib/eq2_tiler.rb
+++ lib/eq2_tiler.rb
@@ -8,3 +8,4 @@ require 'eq2/tiler/tile_bundle'
 require 'eq2/eorath'
 require 'eq2/eorath/eorath_tile_maker'
 
+# TODO reference images inside gem via relativ path constructed from     puts File.dirname(__FILE__)
